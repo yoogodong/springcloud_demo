beee1ac55dce0598ea620e0eb33b3da8fd957722
yoogodong <yoogodong@gmail.com>
2019-03-23 11:56:57 +0800

    ImageApplicaiton is ready

---
+++ image_service/src/main/java/dong/yoogo/springcloud_demo/image_service/ImageApplication.java
@@ -0,0 +1,16 @@
+package dong.yoogo.springcloud_demo.image_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+
+public class ImageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ImageApplication.class,args);
+    }
+}
+
+
