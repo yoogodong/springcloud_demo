f56a394bcc881b6072092eb95a4cab0ca0389596
yoogodong <yoogodong@gmail.com>
2019-03-23 11:34:47 +0800

    init

---
+++ eureka_demo/src/main/java/dong/yoogo/springcloud_demo/eureka/SpringEurekaServerApplication.java
@@ -0,0 +1,11 @@
+package dong.yoogo.springcloud_demo.eureka;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringEurekaServerApplication {
+
+}
