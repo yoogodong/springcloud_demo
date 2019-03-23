2df96109eff12af38093973a94aa3e7c792d2428
yoogodong <yoogodong@gmail.com>
2019-03-23 11:51:38 +0800

    Eureka server ready

--- eureka_demo/src/main/java/dong/yoogo/springcloud_demo/eureka/SpringEurekaServerApplication.java
+++ eureka_demo/src/main/java/dong/yoogo/springcloud_demo/eureka/SpringEurekaServerApplication.java
@@ -1,11 +1,14 @@
 package dong.yoogo.springcloud_demo.eureka;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class SpringEurekaServerApplication {
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEurekaServerApplication.class,args);
+    }
 }
